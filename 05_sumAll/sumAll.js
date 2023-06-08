6663a1051c4b407dba5815e6bd1dffd070f0e771
perazabg <96875802+perazabg@users.noreply.github.com>
2023-06-08 03:30:16 -0700

    finished problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    if(num1 < 0 || num2 < 0){return 'Error';}
+    if(Number.isInteger(num1) && Number.isInteger(num2)){
+      if(num1>num2){
+        for(i=num2;i<=num1;i++){
+          sum = i + sum;
+        }
+        return sum;
+      }
+      else{
+        for(i=num1;i<=num2;i++){
+          sum = i+ sum;
+        }
+        return sum;
+      }
+    }else{
+    return "ERROR";
+    }
+}
+   
+  //console.log(sumAll(3,4));
 
 // Do not edit below this line
 module.exports = sumAll;
