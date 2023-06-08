0c3fb5afe42770702a5f189c2cde767cbcd2dcd3
perazabg <96875802+perazabg@users.noreply.github.com>
2023-06-08 05:22:55 -0700

    idk

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1,num2) {
     let sum = 0;
-    if(num1 < 0 || num2 < 0){return 'Error';}
+    if((num1 < 0) || (num2 < 0)){return 'ERROR';}
     if(Number.isInteger(num1) && Number.isInteger(num2)){
       if(num1>num2){
         for(i=num2;i<=num1;i++){
