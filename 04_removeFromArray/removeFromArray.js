2cb6b0dd188d16996e992f201f1e341d9c59dafa
perazabg <96875802+perazabg@users.noreply.github.com>
2023-06-08 01:34:11 -0700

    completed problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (let i = 0; i < itemsToRemove.length; i++) {
+      const item = itemsToRemove[i];
+      const index = array.indexOf(item);
+      if (index !== -1) {
+        array.splice(index, 1);
+      }
+    }
+  
+    return array;
+  };
+  
+  //console.log(removeFromArray([1,2, 3, 4,5], '2',4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
