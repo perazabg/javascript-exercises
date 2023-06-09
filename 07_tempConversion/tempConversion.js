7e80d889d4a48f49a50fd840a3f2d467c7edb16b
perazabg <96875802+perazabg@users.noreply.github.com>
2023-06-08 19:49:30 -0700

    finished #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) * (5/9);
+  return round(conversion,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = (temp * (9/5)) + 32;
+  return +conversion.toFixed(1);
 };
+
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
